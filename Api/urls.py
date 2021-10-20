dc426e93d041d99cbb2080111feb403e27006fed
Adonis Rumbwere <adonis.rumbwere@gmail.com>
2021-10-20 12:47:00 +0200

    fixed search bug

--- Api/urls.py
+++ Api/urls.py
@@ -6,11 +6,10 @@ from .views import *
 
 urlpatterns = [
 
-    path('people/<peoplename>/',PeopleSearch.as_view(), name="search"),
+    # path('people/<peoplename>/',PeopleSearch.as_view(), name="search"),
     path('peoplepost/',PeoplePost.as_view(), name="ppost"),
     path('peopledelete/',PeopleDelete.as_view(), name="pdelete"),
     path('peopleput/',PeoplePut.as_view(), name="pput"),
-    path('people/',PeopleView.as_view(), name="peopleget" ),
-    path('people',SearchPeople.as_view(), name="peoplesearch" ),
-
+    path('people/',SearchPeople.as_view(), name="peoplesearch" ),
+    path('people/',PeopleView.as_view(), name="peopleget" )
 ]
